ab866450de69438fd524d643b99ea0c671995dad
Kab Thangarajah <kabilanm.thangarajah@gmail.com>
2021-06-08 21:55:01 -0700

    Initial Commit

---
+++ kab-dev/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+//https://colorhunt.co/palette/273948
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content main">
+    <div style="text-align:center" class="content wrapper">
+      <h1>R<span style="color: #f05945">i</span>ck R<span style="color: #f05945">o</span>ll<span style="color: #f05945">e</span>d 🤭</h1>
+      <iframe width="480" height="400" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1" title="Rick Rolled 🤪" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; autoplay" id="rickVid"></iframe>
+      <audio autoplay loop  id="playAudio">
+          <source src="https://www.myinstants.com/media/sounds/epic.mp3">
+      </audio>
+      <div style="margin-top:1%">
+        <img src="https://i.kym-cdn.com/photos/images/original/000/569/146/2c9.gif" alt="BMO Dance" style="width:20%;height:20%;">
+      </div>
+      </div>
+    </div>
+    <router-outlet></router-outlet>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'kab-dev';
+}
